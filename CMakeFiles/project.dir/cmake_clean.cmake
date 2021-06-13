8603309602c3e11a788f1fa45697a936b1e2b0e1
tayyabejaz2000 <tayyabejaz700@gmail.com>
2021-06-13 23:24:07 +0500

    Updated .gitignore

---
+++ CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/src/main.cpp.o"
+  "CMakeFiles/project.dir/src/main.cpp.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
